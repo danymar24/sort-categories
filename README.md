e05189e0f7e59c2da8e2914f77544c4f31c13294
Daniel Rodriguez <danymar24@gmail.com>
2020-11-11 23:35:29 -0600

    Added JSON support, explanation of structure, added loops enhancements

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-* Category Sort Test
+Category Sort Test
 
 Solution located on app.js
+
+The solution is based assuming the input array data tree has the following structure:
+
+[
+    rootCategory,
+        subcategoryLevel1,
+            subcategoryLevel2,
+            subcategoryLevel2,
+        subcategoryLevel1,
+            subcategoryLevel2,
+            subcategoryLevel2,    
+    rootCategory,
+        subcategoryLevel1,
+            subcategoryLevel2,
+            subcategoryLevel2,
+        subcategoryLevel1,
+            subcategoryLevel2,
+            subcategoryLevel2,
+]
--- app.js
+++ app.js
@@ -2,31 +2,30 @@ module.exports = function sortCategoriesForInsert(inputJson) {
     // Your code happens...
     ///   ... which calculates properJsonOutput
 
+    /** Converts the inputJson value to a javascript readable object */
+    const parsedJson = JSON.parse(inputJson);
     var properJsonOutput = [];
-
-    /**
-     * To initialize the sort I find all the root categories, if the parent_id value is null,
-     * it means it is a root category
-    */
-    const rootCategory = inputJson.filter(category => category.parent_id === null);
 
     /**
      * Iterate trough the root categories to get the sub categories
      */
-    for (let i = 0; i < rootCategory.length; i++) {
-        const element = rootCategory[i];
-        /** push the root element to the properJsonOutput */
-        properJsonOutput.push(element);
+    for (let i = 0; i < parsedJson.length; i++) {
+        const element = parsedJson[i];
+        /** If the category is a root category, it will push it to properJsonOutput */
+        if (element.parent_id === null) {
+            /** push the root element to the properJsonOutput */
+            properJsonOutput.push(element);
 
-        /** 
-         * Gets the subcategories passing the id of the element and the array, 
-         * then concatenates the result to the properJsonOutput array 
-         */
-        properJsonOutput = properJsonOutput.concat(findSubCategories(element.id, inputJson));
+            /** 
+             * Gets the subcategories passing the id of the element and the array, 
+             * then concatenates the result to the properJsonOutput array 
+             */
+            properJsonOutput = properJsonOutput.concat(findSubCategories(element.id, parsedJson));
+        }
     }
 
-    /** Returns the sorted array */
-    return properJsonOutput
+    /** Returns the sorted array as JSON string*/
+    return JSON.stringify(properJsonOutput);
 }
 
 /**
@@ -40,24 +39,21 @@ module.exports = function sortCategoriesForInsert(inputJson) {
 function findSubCategories(id, array) {
     let sortedCategories = [];
 
-    /** 
-     * Filters the categories of the given id, 
-     * will assign the array of categories if the value parent_id equals the given id
-     */
-    const subCategoriesFromId = array.filter(category => category.parent_id === id);
     /** Loops trough the subcategories array */
-    for (let i = 0; i < subCategoriesFromId.length; i++) {
-        const element = subCategoriesFromId[i];
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
 
-        /** adds the current category to the sortedCategories array */
-        sortedCategories.push(element);
+        if (element.parent_id === id) {
+            /** adds the current category to the sortedCategories array */
+            sortedCategories.push(element);
 
-        /** 
-         * Finds the sub categories of the current category,
-         * this helps when there are nested subcategories, as it will iterate
-         * until there are no more nested subcategories.
-         */
-        sortedCategories = sortedCategories.concat(findSubCategories(element.id, array));
+            /** 
+             * Finds the sub categories of the current category,
+             * this helps when there are nested subcategories, as it will iterate
+             * until there are no more nested subcategories.
+             */
+            sortedCategories = sortedCategories.concat(findSubCategories(element.id, array));
+        }
     }
 
     return sortedCategories;
